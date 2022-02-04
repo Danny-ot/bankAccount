59358e8799eb1abc1ba77829b8cd7a760fe9fbc1
Danny-ot <93927802+Danny-ot@users.noreply.github.com>
2022-02-04 02:00:55 -0800

    Start Creating the business logic for the bank account

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,28 @@
-function bank(){
+//Constructor For The Bank
+function Bank(){
     this.accounts = {
 
     };
+    this.id = 0;
 }
 
-function account(){
-    this.transactions = {
-        
+//Method For Assigning Id
+
+//Method For Adding Account
+Bank.prototype.addAccount = function(account){
+
+}
+
+//Constructor For Creating Account
+function Account(firstName , lastName , initialDeposit){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.initialDeposit = initialDeposit;
+    this.date = new Date().toLocaleDateString();
+    this.Transactions = {
+        withdrawalAmount : 0,
+        deposit : initialDeposit,
+        balance : 0
     }
 }
+
