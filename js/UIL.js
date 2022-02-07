69752da9fd24a971195386fe44de7cba38b65ff8
Danny-ot <93927802+Danny-ot@users.noreply.github.com>
2022-02-07 02:17:10 -0800

    Create the click function for the transact btn

--- js/UIL.js
+++ js/UIL.js
@@ -1,50 +1,53 @@
 let bank = new Bank();
 
 // Function For Displaying Contents
-function displayContent(account){
+function displayContent(account) {
     let domElement = $("#accounts-info");
     let htmlString = "";
     let keys = Object.keys(account.accounts);
-    keys.forEach(function(key){
+    keys.forEach(function (key) {
         let account = bank.findAccount(key);
-       htmlString += "<li class = 'data mt-3' id = " + account.id + ">" + account.fullName(); + "</li>"
+        htmlString += "<li class = 'data mt-3' id = " + account.id + ">" + account.fullName(); + "</li>"
     })
     domElement.html(htmlString);
 }
 
 
 // Function For attaching the accountlistener
-function attachListener(){
+function attachListener() {
     // Listener For The Li Click
-    $("ul#accounts-info").on("click" , "li" , function(){
+    $("ul#accounts-info").on("click", "li", function () {
+        $(".details-transaction").html("<button class='btn btn-outline-info col-xsm-1 details' type='button'>Details</button>" + "<button class='btn btn-outline-dark col-xsm-2 ml-3 transaction-btn' id =" + this.id + " type='button'>Make Transaction</button>")
         $(".details-transaction").show();
         displayDetails(this.id);
     });
 
     // Listener For The details Button Click
-    $(".details-transaction").on("click" , ".details" , function(){
+    $(".details-transaction").on("click", ".details", function () {
         $(".transaction").hide();
         $("#details-display").toggle();
     });
 
     // Listener For The transactions button click
-    $(".details-transaction").on("click" , ".transaction-btn" , function(){
+    $(".details-transaction").on("click", ".transaction-btn", function () {
         $("#details-display").hide();
+        $("#transact").empty();
+        $("#transact").html(" <button class='btn-outline-info btn transa' id =" + this.id + " type = 'button'>Transact</button>")
         $(".transaction").toggle();
     });
 
 
     // Listener For The delete button click
-    $(".buttons").on("click" , ".deleteButton" , function(){
+    $(".buttons").on("click", ".deleteButton", function () {
         bank.deleteAccount(this.id);
         $("#details-display").hide();
         $(".details-transaction").hide();
         displayContent(bank)
-    }) 
+    })
 }
 
 // function for account details display
-function displayDetails(id){
+function displayDetails(id) {
     let account = bank.findAccount(id);
     $("#firstname-display").html(account.firstName);
     $("#lastname-display").html(account.lastName);
@@ -52,26 +55,26 @@ function displayDetails(id){
     $("#init-depo-display").html(account.initialDeposit);
     let button = $(".buttons");
     button.empty();
-    button.html("<button class = 'btn btn-light deleteButton' id = " +  account.id + ">Delete </button>")
+    button.html("<button class = 'btn btn-light deleteButton' id = " + account.id + ">Delete </button>")
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
     attachListener();
     //Click method for adding account
-    $("#add-account").click(function(){
+    $("#add-account").click(function () {
         $(".accounts").hide();
         $(".register-account-form").toggle();
     });
 
     //Register Account Form Submission
-    $("#register-form").submit(function(event){
+    $("#register-form").submit(function (event) {
         event.preventDefault();
         const firstName = $("#firstname-input").val();
         const lastName = $("#lastname-input").val();
         const dob = $("#dob-input").val();
         const initDeposit = parseInt($("#initdepo-input").val());
 
-        let account = new Account(firstName , lastName , dob , initDeposit);
+        let account = new Account(firstName, lastName, dob, initDeposit);
         bank.addAccount(account);
         $("#firstname-input").val("");
         $("#lastname-input").val("");
@@ -79,22 +82,42 @@ $(document).ready(function(){
         $("#initdepo-input").val("");
         $(".register-account-form").hide();
         displayContent(bank);
-        
+
+
+
     });
 
     // Click Method For Displaying Lists
-    $("#check-account").click(function(){
+    $("#check-account").click(function () {
         $(".register-account-form").hide();
         $(".accounts").toggle();
     })
 
-
     // Form Submission for withdrawal and deposit
-    $("#transaction-form").submit(function(){
+    $("transaction-form").click(function () {
         const withdraw = $("#withdrawal-input").val();
         const deposit = $("#depsit-input").val();
-        if(withdraw !== "" && deposit !== ""){
-            
+        
+        let account = bank.findAccount(key);
+        $("#balance-output").html(account.transactions.balance);
+        if (withdraw !== "" && deposit !== "") {
+            $("#transaction-warning").show();
         }
+        else if (withdraw !== "" && deposit === "") {
+            $("#transaction-warning").hide();
+            account.withDraw(parseInt(withdraw));
+            $("#balance-output").html(account.transactions.balance);
+            $("#withdrawal-input").val("");
+            $("#depsit-input").val("");
+        }
+        else if (withdraw === "" && deposit !== "") {
+            $("#transaction-warning").hide();
+            account.deposit(parseInt(deposit));
+            $("#balance-output").html(account.transactions.balance);
+            $("#withdrawal-input").val("");
+            $("#depsit-input").val("");
+        }
+
     })
+
 })
